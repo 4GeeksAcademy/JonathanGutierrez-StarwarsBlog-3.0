376365895def251e096dce18145c89d40c987fca
Jonathan Gutiérrez <120731494+Goodtierrez@users.noreply.github.com>
2023-05-11 14:39:33 +0000

    starwars blog completed

--- src/js/views/StarwarsCharacters.js
+++ src/js/views/StarwarsCharacters.js
@@ -2,58 +2,59 @@ import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
-
 const StarwarsCharacters = () => {
+  const [characters, setCharacters] = useState([]);
 
-    const [characters, setCharacters] = useState([]);
+  // Obtener todos los personajes
+  useEffect(() => {
+    getAllCharacters();
+  }, []);
 
-    // Obtener todos los personajes
-    useEffect(() => {
-        getAllCharacters();
-    }, []);
+  const getAllCharacters = () => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-    const getAllCharacters = () => {
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
+    fetch("https://www.swapi.tech/api/people", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        // Almacenar los resultados en el estado "characters"
+        console.log("soy el GET : ", result.results);
+        setCharacters(result.results);
+      })
+      .catch((error) => console.log("error", error));
+  };
 
-        fetch("https://www.swapi.tech/api/people", requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                // Almacenar los resultados en el estado "characters"
-                console.log(result.results);
-                setCharacters(result.results);
-            })
-            .catch(error => console.log('error', error));
-    }
-
-    return (
-        <div className="row">
-            {/* Mapear todos los personajes en una card */}
-            {characters.map((character, index) => {
-                return (
-                    <div key={character.uid} className="col-md-3 my-5">
-                        <Link to={`/StarwarsCharacters/${character.uid}`}>
-                            <div className="card" style={{ width: "18rem" }}>
-                                <img className="card-img-top" src={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`} alt={character.name}
-                                    onError={(e) => {
-                                        // Si la imagen no se carga, reemplazarla por una imagen de marcador de posición
-                                        e.target.onerror = null;
-                                        e.target.src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
-                                    }} />
-                                <div className="card-body">
-                                    <p className="card-text">
-                                        {character.name}
-                                    </p>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="row">
+      {/* Mapear todos los personajes en una card */}
+      {characters.map((character, index) => {
+        return (
+          <div key={character.uid} className="col-md-3 my-5">
+            <Link to={`/StarwarsCharacters/${character.uid}`}>
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  className="card-img-top"
+                  src={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`}
+                  alt={character.name}
+                  onError={(e) => {
+                    // Si la imagen no se carga, reemplazarla por una imagen de marcador de posición
+                    e.target.onerror = null;
+                    e.target.src =
+                      "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+                  }}
+                />
+                <div className="card-body">
+                  <p className="card-text">{character.name}</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default StarwarsCharacters;
--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,42 +1,44 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { BsFillTrash3Fill } from 'react-icons/bs';
+import { BsFillTrash3Fill } from "react-icons/bs";
 
 import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
 
 export const Demo = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="row">
-
-			{store.favoritos.map((fav, index) => {
-				return (
-					<div key={fav.uid} className="col-md-3 my-5">
-
-						<div className="card" style={{ width: "18rem" }}>
-
-							<img className="card-img-top" src={`https://starwars-visualguide.com/assets/img/characters/${fav.uid}.jpg`} alt={fav.name}
-
-								onError={(e) => {
-									// Si la imagen no se carga, reemplazarla por una imagen de marcador de posición
-									e.target.onerror = null;
-									e.target.src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
-								}} />
-							<div className="card-body">
-								<p className="card-text">
-									{fav.name}
-								</p>
-								<BsFillTrash3Fill onClick={() => actions.removeFavorite(fav)} className='icon_trash' />
-							</div>
-						</div>
-
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="row">
+      {store.favoritos.map((fav, index) => {
+        return (
+          <div key={fav.uid} className="col-md-3 my-5">
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                className="card-img-top"
+                src={`https://starwars-visualguide.com/assets/img/characters/${
+                  index + 1
+                }.jpg`}
+                alt={fav.name}
+                onError={(e) => {
+                  // Si la imagen no se carga, reemplazarla por una imagen de marcador de posición
+                  e.target.onerror = null;
+                  e.target.src =
+                    "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+                }}
+              />
+              <div className="card-body">
+                <p className="card-text">{fav.name}</p>
+                <BsFillTrash3Fill
+                  onClick={() => actions.removeFavorite(fav)}
+                  className="icon_trash"
+                />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-
